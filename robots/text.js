aed3f39cab0b9568889d89a9db877655a143374f
Guilherme Silva <guilherme.silva@empow.com.br>
2023-08-11 22:06:54 -0300

    Feat: add Watson natural language understanding service

--- robots/text.js
+++ robots/text.js
@@ -1,9 +1,22 @@
 const wiki = require('wikijs').default;
 const sentenceBoundaryDetection = require('sbd');
+const watsonNLUApiKey = require('../credentials/watson-nlu.json').apikey;
+const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+const nlu = new NaturalLanguageUnderstandingV1({
+  authenticator: new IamAuthenticator({ apikey: watsonNLUApiKey }),
+  version: '2018-04-05',
+  serviceUrl: 'https://api.us-south.natural-language-understanding.watson.cloud.ibm.com'
+});
+
+
 
 async function robot(content) {
     await fetchContentFromWikipedia(content);
     breakContentIntoSentences(content);
+    limitMaximumSentences(content);
+    await fetchKeywordsOfAllSentences(content);
 
     async function fetchContentFromWikipedia(content) {
         const wikiepediaContent = await wiki().page(content.searchTerm)
@@ -31,6 +44,34 @@ async function robot(content) {
             })
         });
     }
-}
+
+    function limitMaximumSentences(content) {
+        content.sentences = content.sentences.slice(0, content.maximumSentences);
+    }
+
+    async function fetchWatsonAndReturnKeywords(sentence) {
+        return new Promise((resolve, reject) => {
+            nlu.analyze({
+                text: sentence,
+                features: {
+                    keywords: {}
+                }
+            })
+            .then(response => {
+                const keywords = response.result.keywords.map((keyword) => {
+                    return keyword.text;
+                });
+
+                resolve(keywords);
+            }); 
+        })
+    }
+
+    async function fetchKeywordsOfAllSentences(content) {
+        for (const sentence of content.sentences) {
+            sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text);
+        }
+    }
+} 
 
 module.exports = robot;
